aaca93a33a283993279a07add5ee5498694a2562
1yimeng <yimwang6@gmail.com>
2021-07-08 23:26:20 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+    const cardArray = [
+        {
+            name: 'black',
+            img: 'images/black.png'  
+        },
+        {
+            name: 'blue',
+            img: 'images/blue.png'  
+        },
+        {
+            name: 'green',
+            img: 'images/green.png'  
+        },
+        {
+            name: 'orange',
+            img: 'images/orange.png'  
+        },
+        {
+            name: 'purple',
+            img: 'images/purple.png'  
+        },
+        {
+            name: 'read',
+            img: 'images/red.png'  
+        },
+        {
+            name: 'white',
+            img: 'images/white.png'  
+        },
+        {
+            name: 'yellow',
+            img: 'images/yellow.png'  
+        }
+        
+    ]
+})

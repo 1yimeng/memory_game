9a283685814ee563c5ee29423adfcd45e1611bc2
1yimeng <yimwang6@gmail.com>
2021-07-11 23:46:35 -0600

    fixed

--- app.js
+++ app.js
@@ -3,37 +3,113 @@ document.addEventListener('DOMContentLoaded', () => {
     //card options
     const cardArray = [
         {
-            name: 'black',
-            img: 'images/black.png'  
+            name: 'blue',
+            img: 'img/blue.png'  
         },
         {
             name: 'blue',
-            img: 'images/blue.png'  
+            img: 'img/blue.png'  
         },
         {
             name: 'green',
-            img: 'images/green.png'  
+            img: 'img/green.png'  
+        },
+        {
+            name: 'green',
+            img: 'img/green.png'  
         },
         {
             name: 'orange',
-            img: 'images/orange.png'  
+            img: 'img/orange.png'  
+        },
+        {
+            name: 'orange',
+            img: 'img/orange.png'  
         },
         {
             name: 'purple',
-            img: 'images/purple.png'  
+            img: 'img/purple.png'  
         },
         {
-            name: 'read',
-            img: 'images/red.png'  
+            name: 'purple',
+            img: 'img/purple.png'  
         },
         {
-            name: 'white',
-            img: 'images/white.png'  
+            name: 'red',
+            img: 'img/red.png'  
+        },
+        {
+            name: 'red',
+            img: 'img/red.png'  
         },
         {
             name: 'yellow',
-            img: 'images/yellow.png'  
+            img: 'img/yellow.png'  
+        },
+        {
+            name: 'yellow',
+            img: 'img/yellow.png'  
         }
-        
     ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector(".grid")
+    const resultDisplay = document.getElementById('result')
+    var cards_chosen = []
+    var cardsChosenId = []
+    var cardsWon = []
+
+    function create_board() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'img/black.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flip_card)
+            grid.append(card)
+        }
+    }
+
+
+    //check for matches
+    function checkForMatch() {
+        cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (optionOneId == optionTwoId) {
+            alert('You have clicked the same image!')
+        } else if (cards_chosen[0] == cards_chosen[1]) {
+            alert('You found a match!')
+            cards[optionOneId].setAttribute('src', 'img/white.png')
+            cards[optionTwoId].setAttribute('src', 'img/white.png')
+            cardsWon.push(cardsChosen)
+            cards[optionOneId].removeEventListener('click', flip_card)
+            cards[optionTwoId].removeEventListener('click', flip_card)
+        } else {
+            cards[optionOneId].setAttribute('src', 'img/black.png')
+            cards[optionTwoId].setAttribute('src', 'img/black.png')
+            alert('Sorry, try again')
+        }
+        cardsChosenId = []
+        cards_chosen = []
+        alert('the card chosen list has been emptied')
+        resultDisplay.innerHTML = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = "Congratualtions, you won!"
+        }
+    }
+
+    //flip your card
+    function flip_card() {
+        let card_ID = this.getAttribute('data-id')
+        cards_chosen.push(cardArray[card_ID].name)
+        cardsChosenId.push(card_ID)
+        this.setAttribute('src', cardArray[card_ID].img)
+        if (cards_chosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    create_board()
+
 })
